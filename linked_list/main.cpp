edb35b6fe3f5db048b4e7d7bd67180980d7c8ac2
shridhar <shridharbenni7@gmail.com>
2024-03-04 11:01:32 +0530

    Dynamic memory
    linked list is created by using dynamic memory allocation

--- linked_list/main.cpp
+++ linked_list/main.cpp
@@ -8,29 +8,28 @@ struct node{
 };
 
 int main(){
-	node n1;
-	node n2;
-	node n3;
+	node *n1, *n2, *n3;
+	n1 = new node();
+	n2 = new node();
+	n3 = new node();
 
-	n1.val = 1;
-	n1.next = &n2; 
-	n2.val = 2;
-	n2.next = &n3;
-	n3.val = 3;
-	n3.next = NULL;
+	n1->val = 1;
+	n1->next = n2; 
+	n2->val = 2;
+	n2->next = n3;
+	n3->val = 3;
+	n3->next = NULL;
 
-	cout<<"n1.val: "<<n1.val<<endl;
-	cout<<"n2.val: "<<n2.val<<endl;
-	cout<<"n3.val: "<<n3.val<<endl;
-	cout<<"n1.next: "<<n1.next<<" &n2: "<<&n2<<endl;
-	cout<<"n2.next: "<<n2.next<<" &n3: "<<&n3<<endl;
-	cout<<"n3.next: "<<n3.next<<endl;
-	
-	cout<<"n1.val: "<<n1.val<<endl;
-	//cout<<"*(n1.next).val: "<<*(n1.next).val<<endl; // error *(nl.next) = *(&n2)
-	cout<<"(*n1.next).val: "<<(*n1.next).val<<endl; // (*&n2).val
-	//cout<<"*(*(n1.next).next).val: "<<*(*(n1.next).next).val<<endl; // error *(*(&n2).next).val 
-	cout<<"(*(*n1.next).next).val: "<<(*(*n1.next).next).val<<endl;
+	cout<<"(*n1).val: "<<(*n1).val<<" n1->val: "<<n1->val<<endl;
+	cout<<"(*n2).val: "<<(*n2).val<<" n2->val: "<<n2->val<<endl;
+	cout<<"(*n3).val: "<<(*n3).val<<" n3->val: "<<n3->val<<endl;
+	cout<<"n1: "<<n1<<endl;
+	cout<<"n2: "<<n2<<" n1->next: "<<n1->next<<endl;
+	cout<<"n3: "<<n3<<" n2->next: "<<n2->next<<endl;
+	cout<<"n3->next: "<<n3->next<<endl;
+	cout<<"n1->val: "<<n1->val<<endl;
+	cout<<"n1->next->val: "<<n1->next->val<<endl;
+	cout<<"n1->next->next->val: "<<n1->next->next->val<<endl;
 	cout<<endl;
 	return 0;
 }
